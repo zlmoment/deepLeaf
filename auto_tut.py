7adddaae26d92a6053046353480f0f91217f5ecf
Max Highsmith <maxrhighsmith@gmail.com>
2017-04-20 08:05:36 -0500

    typo fix

--- auto_tut.py
+++ auto_tut.py
@@ -1,47 +1,41 @@
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
-
+from keras import backend as K
 from scipy.misc import imread, imresize
 
 from keras import regularizers
 
 encoding_dim =32;
 
-input_img = Input(shape =(784,))
-#input_img = Input(shape =(1585,))
+input_img = Input(shape =(28, 28,1))
+x=  Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-#encoded = Dense(encoding_dim, activation='relu',
-#activity_regularizer=regularizers.l1(10e-5))(input_img)
+# at this point the representation is (4, 4, 8) i.e. 128-dimensional
 
-
-#multilayer
-encoded = Dense(128, activation='relu')(input_img)
-encoded = Dense(64, activation='relu')(encoded)
-encoded = Dense(32, activation='relu')(encoded)
-
-
-decoded = Dense(64, activation ='relu')(encoded)
-decoded = Dense(128, activation='relu')(decoded)
-decoded = Dense(784, activation='sigmoid')(decoded)
-#decoded = Dense(1585, activation='sigmoid')(encoded)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(16, (3, 3), activation='relu')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
 autoencoder = Model(input_img, decoded)
-
-encoder = Model(input_img,encoded)
-
-encoded_input = Input(shape=(encoding_dim,))
-
-decode_layer = autoencoder.layers[-1]
-decoder = Model(encoded_input, decode_layer(encoded_input))
-
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+
 
 from keras.datasets import mnist
 import pandas
 import numpy as np
 import os, os.path
 
-(x_train, _), (x_test, _) = mnist.load_data()
+#(x_train, _), (x_test, _) = mnist.load_data()
 
 image_data=imresize(imread('./images/1.jpg'),(28,28)).astype(np.float32)
 image_data=np.expand_dims(image_data,axis=0)
@@ -51,41 +45,59 @@ for img in os.listdir('./images'):
 	image_data = np.vstack((image_data, resized_img))
 
 print(image_data.shape)
-print('shape')
-print(x_train.shape)
 x_train = image_data
 x_test = image_data
 x_train = x_train.astype('float32')/255
 x_test = x_test.astype('float32')/255
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1)) 
 print(x_train.shape)
 print(x_test.shape)
 
 
+from keras.callbacks import TensorBoard
 
 autoencoder.fit(x_train, x_train, epochs=100, batch_size=256, shuffle=True,
-validation_data=(x_test, x_test))
-encoded_imgs = encoder.predict(x_test)
-decoded_imgs = decoder.predict(encoded_imgs)
+validation_data=(x_test, x_test),
+callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+
+#encoded_imgs = encoder.predict(x_test)
+#decoded_imgs = decoder.predict(x_test)
 
 import matplotlib.pyplot as plt
 
-n=10
-plt.figure(figsize=(20,4))
-for i in range(n):
-	ax = plt.subplot(2,n,i+1)
-	plt.imshow(x_test[i].reshape(28,28))
-	plt.gray()
-	ax.get_xaxis().set_visible(False)
-	ax.get_yaxis().set_visible(False)
 
-	ax = plt.subplot(2,n, i+1+n)
-	plt.imshow(decoded_imgs[i].reshape(28,28))
-	plt.gray()
-	ax.get_xaxis().set_visible(False)
-	ax.get_yaxis().set_visible(False)
+
+
+decoded_imgs = autoencoder.predict(x_test)
+
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i+1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + n+1)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 plt.show()
+
+
+
+
+
+
+
+
+
+
 
 
 
